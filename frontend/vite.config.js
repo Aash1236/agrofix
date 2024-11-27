71ca95c4ab0dd2b3058cecda38b1cc4b38ddf100
Aash1236 <ashutoshfase1028@gmil.com>
2024-11-28 01:02:03 +0530

    update

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -3,6 +3,11 @@ import react from "@vitejs/plugin-react";
 
 // https://vite.dev/config/
 export default defineConfig({
+  define: {
+    __DEFINES__: JSON.stringify(
+      process.env.VITE_BACKEND_URL || "default_value"
+    ),
+  },
   plugins: [react()],
   server: { port: 5173 },
 });
